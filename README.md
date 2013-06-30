827b0e901a3f5eb4a68edf0726baf293cd4e931c
Jon Eisen <jeisen@underarmour.com>
2013-06-30 13:43:53 -0400

    Initial Commit

---
+++ README.md
@@ -0,0 +1,64 @@
+# marked-sections
+
+A simple node.js package to use [marked](https://github.com/chjj/marked) to parse markdown and insert `<section>` tags around heading levels.
+
+## Usage
+
+```javascript
+var mdsec = require('marked-sections');
+
+mdsec.setOptions({
+	levels: 2,
+	heirarchy: false,
+	// marked options too
+	gfm: true // see marked for full docs
+});
+
+html = mdsec.parse(markdownText);
+
+// Or you can do it manually
+tree = mdsec.marked.lexer(markdownText);
+sectree = mdsec.insertSections(tree, { levels: 1 });
+html = mdsec.marked.parser(sectree);
+```
+
+## Options
+
+`marked-sections` takes two options above and beyond marked.
+
+- `levels` refers to the header levels to wrap in sections. It should be the highest header level that should be wrapped in a section.
+- `heirarchy` will make each level of header wrapped in a heirarchy of section tags. Defaults to false. For example:
+
+	```
+	# H1
+	something
+	## H2
+	else
+	# H1
+	again
+	```
+
+	becomes
+
+	```
+	<section>
+		<section>
+			<h1>H1</h1>
+			<p>something</p>
+		</section>
+		<section>
+			<h2>H2</h2>
+			<p>else</p>
+		</section>
+	</section>
+	<section>
+		<section>
+			<h1>H1</h1>
+			<p>again</p>
+		</section>
+	</section>
+	```
+
+## License
+
+MIT Licensed 2013 Jon Eisen.
---
+++ test/marked-section_test.js
@@ -0,0 +1,45 @@
+
+var mdsec = require('../lib/marked-sections');
+
+var tests = module.exports = {};
+
+tests.testBasicUse = function (test) {
+	var text = '## header2\n- l1\n- l2';
+	var output = mdsec.parse(text).replace(/[\n\t]/g, '');
+
+	test.equal('<section><h2>header2</h2><ul><li>l1</li><li>l2</li></ul></section>', output);
+	test.done();
+}
+
+tests.testMultiHeader = function (test) {
+	var text = '# head1\ntest\n\n## head2\n- l1\n- l2\n\n# head12';
+	var output = mdsec.parse(text).replace(/[\n\t]/g, '');
+
+	test.equal('<section><h1>head1</h1><p>test</p></section><section><h2>head2</h2><ul><li>l1</li><li>l2</li></ul></section><section><h1>head12</h1></section>', output);
+	test.done();
+}
+
+tests.testHeaderLevel = function (test) {
+	var text = '# head1\ntest\n\n## head2\n- l1\n- l2\n\n# head12';
+	var output = mdsec.parse(text, {levels: 1}).replace(/[\n\t]/g, '');
+
+	test.equal('<section><h1>head1</h1><p>test</p><h2>head2</h2><ul><li>l1</li><li>l2</li></ul></section><section><h1>head12</h1></section>', output);
+	test.done();
+}
+
+tests.testHeaderHeirarchy = function (test) {
+	var text = '# head1\ntest\n\n## head2\n- l1\n- l2\n\n# head12';
+	var output = mdsec.parse(text, {levels: 2, heirarchy: 2}).replace(/[\n\t]/g, '');
+
+	test.equal('<section><section><h1>head1</h1><p>test</p></section><section><h2>head2</h2><ul><li>l1</li><li>l2</li></ul></section></section><section><section><h1>head12</h1></section></section>', output);
+	test.done();
+}
+
+tests.testSetOptions = function (test) {
+	mdsec.setOptions({ levels: 1, heirarchy: false });
+	var text = '# head1\ntest\n\n## head2\n- l1\n- l2\n\n# head12';
+	var output = mdsec.parse(text).replace(/[\n\t]/g, '');
+
+	test.equal('<section><h1>head1</h1><p>test</p><h2>head2</h2><ul><li>l1</li><li>l2</li></ul></section><section><h1>head12</h1></section>', output);
+	test.done();
+}
